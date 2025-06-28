0126cfa263c523d463b3397c558131ec74e9f50d
dbochoa77 <dbochoa77@proton.me>
2025-06-28 14:51:15 -0700

    added home whre i worked on the default.nix

---
+++ home/dbochoa77/default.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  outputs,
+  pkgs,
+  ...
+}: {
+  nixpkgs = {
+    # Overlays
+    overlays = [
+    outputs.overlays.additions
+    outputs.overlays.modifications
+    outputs.overlays.stable-packages
+
+    ];
+
+    config = {
+    allowUnfree = true;
+
+    allowUnfreePredicate = _: true;
+  };
+};
+
+  nix - {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental=features = ["nix-command" "flakes"];
+      warn-dirty = false;
+    };
+  };
+}
