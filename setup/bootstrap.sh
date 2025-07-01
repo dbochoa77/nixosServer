6d064a945cb1082c9ff8d302bc058411e2047125
dbochoa77 <dbochoa77@proton.me>
2025-07-01 16:19:28 -0700

    updated bootstrap.sh

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -5,10 +5,10 @@ ping -c 4 google.com &
 git clone https://github.com/dbochoa77/nixosServer
 wait
 
-rm /home/dbochoa77/nixosServer/hosts/hardware-configuration.nix
-wait
-
 cp /etc/nixos/hardware-configuration.nix ~/nixosServer/hosts/
+
+rm -f /home/dbochoa77/nixosServer/hosts/hardware-configuration.nix
+wait
 
 sudo nixos-rebuild switch --flake ~/nixosServer#nixosServer
 home-manager switch --flake ~/nixosServer#nixosServer
