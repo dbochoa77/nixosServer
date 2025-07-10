48f4ec31645c965eea6dfc310c385fffcef9a0bd
dbochoa77 <dbochoa77@proton.me>
2025-07-09 17:10:14 -0700

    Updated @ inputs: let     inherit (self) outputs;     systems = [       aarch64-linux       i686-linux       x86_64-linux       aarch64-darwin       x86_64-darwin     ]; in flake.nix

--- flake.nix
+++ flake.nix
@@ -25,6 +25,10 @@
       inherit (self) outputs;
       systems = [
         "x86_64-linux"
+	"i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
       ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
   
